a84be4247eba72f2a845d63c4a86bb28b6118375
Efarov Ilya <efarov.ily@mail.ru>
2021-01-09 18:20:16 +0900

    fetch LevelFileParser

---
+++ Grinch2D/Assets/Scripts/GameSceneHandler.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+/*
+ * class GameSceneHandler
+ * Init all action on game scene (generate level on game scene, etc.)
+ */
+
+public class GameSceneHandler : MonoBehaviour
+{
+    public Vector2 blockSize;
+    public Vector2 maxLevelSize;                    // max level height (count blocks)
+    public string levelDictPath;                    // path for level dictionary file
+    public string levelsPath;                       // path for levels directory
+    private int current_level = 1;                  // index current running level
+    private LevelFileParser fileParser;             // parser level files
+
+    public int currentLevel
+    {
+        get { return currentLevel; }
+        set { ConstructLevel(current_level); }
+    }
+
+    void Start()
+    {
+        fileParser = GetComponent<LevelFileParser>();
+        currentLevel = 1;
+    }
+
+    public void ConstructLevel(int level)
+    {
+        Debug.Log("Kek");
+        current_level = level;
+        fileParser.parseLevelDict(levelDictPath);
+        fileParser.parseLevelFile(levelsPath, level, (int)maxLevelSize.y, (int)maxLevelSize.x);
+
+        foreach (KeyValuePair<char, string> pair in fileParser.levelDict)
+        {
+            Debug.Log(pair.Key.ToString() + " : " +  pair.Value.ToString());
+        }
+
+        if (fileParser.levelMap == null) return;
+
+        for (int i = 0; i < (int)fileParser.mapSize.y; i++)
+        {
+            string answer = "";
+            for (int j = 0; j < (int)fileParser.mapSize.x; j++)
+            {
+                answer += fileParser.levelMap[i, j].ToString();
+            }
+            Debug.Log(answer);
+        }
+    }
+}
---
+++ Grinch2D/Assets/Scripts/LevelFileParser.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFileParser : MonoBehaviour
+{
+    private Dictionary<char, string> level_dict;
+    private char[,] level_map;
+    private Vector2 map_size;
+
+    public Dictionary<char, string> levelDict { get { return level_dict; } }
+    public char[,] levelMap { get { return level_map; } }
+    public Vector2 mapSize { get { return map_size; } }
+
+    void Start()
+    {
+        level_dict = new Dictionary<char, string>();
+        level_map = null;
+        map_size = new Vector2(0, 0);
+    }
+
+    // Function parse file with level dictionary
+    // example answer: {'i': 'ice_block', 's': 'stone_block', ... }
+    public void parseLevelDict(string levelDictPath)
+    {
+        level_dict.Clear();
+        if (!File.Exists(levelDictPath)) return;                                    // check file exists
+
+        using (StreamReader sr = File.OpenText(levelDictPath))                      // open text file with level dictionary
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)                                  // load data of file
+            {
+                string[] pair = line.Split(':');
+                if (pair[0].Length == 0) continue;
+                level_dict.Add(pair[0][0], pair[1].Split(' ')[1]);
+            }
+        }
+    }
+
+    // Function parse file with level map
+    /* example answer: 
+     * ssssssssssssssssssssssssssssssssssss
+     * s                                  s
+     * s                  p               s
+     * s                  s               s
+     * s                  ss              s
+     * ssssssssssssssssssssssssssssssssssss
+     */
+    public void parseLevelFile(string levelsPath, int ind_level, int max_h, int max_w)
+    {
+        level_map = null;
+        map_size = new Vector2(0, 0);
+        if (max_h <= 0 || max_w <= 0) return;
+
+        string levelPath = levelsPath + "/" + ind_level.ToString() + ".txt";
+        if (!File.Exists(levelPath)) return;                                    // check file exists
+
+        level_map = new char[max_h, max_w];                                     // create map array
+
+        using (StreamReader sr = File.OpenText(levelPath))                      // open text file with level dictionary
+        {
+            string line;
+            int row = 0;
+            int max_len_line = -1;                                              // max line length in file
+            while ((line = sr.ReadLine()) != null && row < max_h)               // load map of file
+            {
+                for (int col = 0; col < line.Length && col < max_w; col++)      // load map line of file line
+                    level_map[row, col] = line[col];
+                row++;
+                if (max_len_line < line.Length)
+                    max_len_line = line.Length;
+            }
+            map_size.y = row;                                                   // store fact map size
+            map_size.x = max_len_line;                  
+        }
+    }
+}
