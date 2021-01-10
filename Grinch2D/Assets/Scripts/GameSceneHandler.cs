2aaa59552d3d3d3ae2a8bbbc135fceea2971cf8b
Efarov Ilya <efarov.ily@mail.ru>
2021-01-10 10:15:05 +0900

    fix LevelFileParser, doc LevelFileParser

--- Grinch2D/Assets/Scripts/GameSceneHandler.cs
+++ Grinch2D/Assets/Scripts/GameSceneHandler.cs
@@ -2,28 +2,28 @@
 using System.Collections.Generic;
 
 
-/*
- * class GameSceneHandler
- * Init all action on game scene (generate level on game scene, etc.)
- */
 
+/// <summary>
+/// class GameSceneHandler
+/// is init all action on game scene (generate level on game scene, etc.)
+/// </summary>
 public class GameSceneHandler : MonoBehaviour
 {
+    public GameObject blockSample;                  // block sample with box colider2d for generating other blocks
     public GameObject blocksField;                  // field for created blocks
     public GameObject[] blockPrefabs;               // block prefab samples
     public Vector2 maxLevelSize;                    // max level height (count blocks)
+
     public string levelDictPath;                    // path for level dictionary file
     public string levelsPath;                       // path for levels directory
-    private int current_level = 1;                  // index current running level
     private LevelFileParser fileParser;             // parser level files
+
+    private int current_level = 1;                  // index current running level
 
     public int currentLevel
     {
         get { return currentLevel; }
-        set 
-        { 
-            ConstructLevel(value); 
-        }
+        set { ConstructLevel(value); }
     }
 
     void Start()
@@ -32,55 +32,42 @@ public class GameSceneHandler : MonoBehaviour
         currentLevel = 1;
     }
 
-    // Function create level by level files and level dictionary file
+    /// <summary>
+    /// Method create level in gamescene by level files and level dictionary file
+    /// </summary>
+    /// <param name="level"> level number </param>
     public void ConstructLevel(int level)
     {
         current_level = level;
-        fileParser.parseLevelDict(levelDictPath);                                                     // get level dict
-        fileParser.parseLevelFile(levelsPath, level, (int)maxLevelSize.y, (int)maxLevelSize.x);       // get level map
-        GenerateLevelObjects(fileParser.levelDict, fileParser.levelMap, fileParser.mapSize);
-
-        // ========================= Debug ================================
-        foreach (KeyValuePair<char, string> pair in fileParser.levelDict)
-        {
-            Debug.Log(pair.Key.ToString() + " : " +  pair.Value.ToString());
-        }
-
-        if (fileParser.levelMap == null) return;
-
-        Debug.Log(fileParser.levelBackground);
-
-        for (int i = 0; i < (int)fileParser.mapSize.y; i++)
-        {
-            string answer = "";
-            for (int j = 0; j < (int)fileParser.mapSize.x; j++)
-            {
-                answer += fileParser.levelMap[i, j].ToString();
-            }
-            Debug.Log(answer);
-        }
-
-        Debug.Log("size: (" + fileParser.mapSize.x.ToString() + "; " + fileParser.mapSize.y.ToString() + ")");
-        // ========================= Debug ================================
+        fileParser.parseLevelDict(levelDictPath);
+        fileParser.parseLevelFile(levelsPath, level, (int)maxLevelSize.y, (int)maxLevelSize.x);
+        CreateLevelObjsByMap(fileParser.levelDict, fileParser.levelMap, fileParser.mapSize);
     }
 
-    private void GenerateLevelObjects(Dictionary<char, string> level_dict, char[,] level_map, Vector2 map_size)
+    /// <summary>
+    /// Method create level objects by level dictionary and level map
+    /// </summary>
+    /// <param name="level_dict"> level dictionary </param>
+    /// <param name="level_map"> level map </param>
+    /// <param name="map_size"> level map size </param>
+    private void CreateLevelObjsByMap(Dictionary<char, string> level_dict, char[,] level_map, Vector2 map_size)
     {
         if (blockPrefabs.Length <= 0) return;
-        BoxCollider2D blockBox = blockPrefabs[0].GetComponent<BoxCollider2D>();
-        if (blockBox == null) return;
 
-        for (int i = (int)map_size.y - 1; i >= 0; i--)
+        BoxCollider2D blockBoxSample = blockSample.GetComponent<BoxCollider2D>();
+        if (blockBoxSample == null) return;
+
+        for (int i = (int)map_size.y - 1; i >= 0; i--)                                      // reverse step, because file with map was readed top down
         {
-            for (int j = (int)map_size.x - 1; j >= 0; j--)
+            for (int j = 0; j < (int)map_size.x; j++)
             {
                 string prefabName;
-                if (!level_dict.TryGetValue(level_map[i, j], out prefabName)) continue;
+                if (!level_dict.TryGetValue(level_map[i, j], out prefabName)) continue;     // get prefab name of level dictionary
 
-                float x = ((int)map_size.x - 1 - j) * blockBox.size.x / 2;
-                float y = ((int)map_size.y - 1 - i) * blockBox.size.y / 2;
+                float x = j * blockBoxSample.size.x / 2;                                    // calculate position for instantiate prefab object
+                float y = ((int)map_size.y - 1 - i) * blockBoxSample.size.y / 2;
 
-                GameObject block = null;
+                GameObject block = null;                                                    // search prefab object by name
                 foreach (GameObject blockPrefab in blockPrefabs)
                 {
                     if (blockPrefab.name == prefabName)
@@ -90,8 +77,9 @@ public class GameSceneHandler : MonoBehaviour
                     }
                 }
                 if (block == null) continue;
-
-                GameObject blockObject =
+                                                                 
+                // Create block and add it in blocks field object.
+                GameObject blockObject =                                                
                     Instantiate(block, new Vector3(x, y, block.transform.position.z), Quaternion.identity) as GameObject;
                 blockObject.transform.parent = blocksField.transform;
             }
--- Grinch2D/Assets/Scripts/LevelFileParser.cs
+++ Grinch2D/Assets/Scripts/LevelFileParser.cs
@@ -3,88 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-// class LevelFileParser
-// is a parser level files.
+/// <summary>
+/// class LevelFileParser 
+/// is a parser level files data (level dict, level map and etc.).
+/// </summary>
 public class LevelFileParser : MonoBehaviour
 {
+    /* example level dict: {'i': 'ice_block', 's': 'stone_block', ... }
+     * example level map, byte matrix:
+     * ssssssssssssssssssssssssssssssssssss
+     * s                                  s
+     * s    sss           p               s
+     * s                  s               s
+     * s                 sss              s
+     * ssssssssssssssssssssssssssssssssssss
+     */
     private Dictionary<char, string> level_dict;
     private char[,] level_map;
     private Vector2 map_size;
     private char level_bg;
 
-    public Dictionary<char, string> levelDict { get { return level_dict; } }            // level dictionary, example dict: {'i': 'ice_block', 's': 'stone_block', ... }
-    public char[,] levelMap { get { return level_map; } }                               // level map
-    /* example level map:
-     * ssssssssssssssssssssssssssssssssssss
-     * s                                  s
-     * s                  p               s
-     * s                  s               s
-     * s                  ss              s
-     * ssssssssssssssssssssssssssssssssssss
-     */
-    public Vector2 mapSize { get { return map_size; } }                                 // level map size, (count rows, count columns)
+    public Dictionary<char, string> levelDict { get { return level_dict; } }            // level dictionary
+    public char[,] levelMap { get { return level_map; } }                               // level map, blocks matrix
+    public Vector2 mapSize { get { return map_size; } }                                 // size map matrix, (x: count columns, y: count rows)
     public char levelBackground { get { return level_bg; } }                            // level background, background key
 
-    void Awake()
-    {
-        level_dict = new Dictionary<char, string>();
-        level_map = null;
-        map_size = new Vector2(0, 0);
-        level_bg = '0';
-    }
-
-    // Function parse file with level dictionary
+    /// <summary>
+    /// Method parse file with level dictionary
+    /// </summary>
+    /// <param name="levelDictPath"> path to the file with level dictionary </param>
     public void parseLevelDict(string levelDictPath)
     {
         if (level_dict == null)
             level_dict = new Dictionary<char, string>();
         else
             level_dict.Clear();
+
         if (!File.Exists(levelDictPath)) return;                                    // check file exists
 
         using (StreamReader sr = File.OpenText(levelDictPath))                      // open text file with level dictionary
         {
             string line;
-            while ((line = sr.ReadLine()) != null)                                  // load data of file
+            while ((line = sr.ReadLine()) != null)                                  // load line of file
             {
                 string[] pair = line.Split(':');
                 if (pair[0].Length == 0) continue;
-                level_dict.Add(pair[0][0], pair[1].Split(' ')[1]);
+                level_dict.Add(pair[0][0], pair[1].Split(' ')[1]);                  // load pair of file, example: "e : evil_block"
             }
         }
     }
 
-    // Function parse file with level map
+    /// <summary>
+    /// Method parse file with level map
+    /// </summary>
+    /// <param name="levelsPath"> path to the file with level map </param>
+    /// <param name="ind_level"> level number </param>
+    /// <param name="max_h"> max blocks count in height </param>
+    /// <param name="max_w"> max blocks count in width </param>
     public void parseLevelFile(string levelsPath, int ind_level, int max_h, int max_w)
     {
         level_map = null;
         map_size = new Vector2(0, 0);
-        if (max_h <= 0 || max_w <= 0) return;
+        if (max_h <= 0 || max_w <= 0) return;                                   // check params correctness
 
         string levelPath = levelsPath + "/" + ind_level.ToString() + ".txt";
         if (!File.Exists(levelPath)) return;                                    // check file exists
 
-        level_map = new char[max_h, max_w];                                     // create map array
+        level_map = new char[max_h, max_w];
 
-        using (StreamReader sr = File.OpenText(levelPath))                      // open text file with level dictionary
+        using (StreamReader sr = File.OpenText(levelPath))                      // open text file with level map
         {
             string line;
             if ((line = sr.ReadLine()) == null) return;
             
-            if (line.Length > 0) level_bg = line[0];
+            if (line.Length > 0) level_bg = line[0];                            // the first byte is a value level background
 
-            int row = 0;
-            int max_len_line = -1;                                              // max line length in file
-            while ((line = sr.ReadLine()) != null && row < max_h)               // load map of file
+            int row;
+            int file_max_w = 0;                                                 // max level width in file
+            for (row = 0; (line = sr.ReadLine()) != null && row < max_h; row++)
             {
-                for (int col = 0; col < line.Length && col < max_w; col++)      // load map line of file line
+                for (int col = 0; col < line.Length && col < max_w; col++)
                     level_map[row, col] = line[col];
-                row++;
-                if (max_len_line < line.Length)                                 // find max line length in file
-                    max_len_line = line.Length;
+
+                if (file_max_w < line.Length)                                  // find max level width in file
+                    file_max_w = line.Length;
             }
-            map_size.y = row;                                                   // store fact map size
-            map_size.x = max_len_line;                  
+            map_size.y = row;                                                  // store fact map size
+            map_size.x = file_max_w;                  
         }
     }
 }
