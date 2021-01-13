e10a19d04adefc0ea0716ee26df6ce6165a91efe
Efarov Ilya <efarov.ily@mail.ru>
2021-01-13 21:53:03 +0900

    fix player jump

--- Grinch2D/Assets/Scripts/PlayerMoveHandler.cs
+++ Grinch2D/Assets/Scripts/PlayerMoveHandler.cs
@@ -7,13 +7,21 @@ using UnityEngine;
 /// </summary>
 public class PlayerMoveHandler : JumpHandler
 {
+    private bool in_air = false;                                    // player in air?
+
+    /// <summary>
+    /// Method contains code for updating current move direction
+    /// </summary>
     protected override void UpdateDirection()
     {
         float inputx = Input.GetAxis("Horizontal");                 // change move direction on left or right
         direction.x = inputx;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !in_air)
+        {
             Jump();                                                 // peform jump
+            in_air = true;
+        }
 
         base.UpdateDirection();
     }
@@ -25,5 +33,21 @@ public class PlayerMoveHandler : JumpHandler
     {
         Rigidbody2D rgbody = GetComponent<Rigidbody2D>();
         rgbody.MovePosition(rgbody.position + speed * Time.deltaTime);
+    }
+
+    void OnCollisionStay2D(Collision2D collisions)
+    {
+        for (int i = 0; i < collisions.contactCount; i++)
+        {
+            if (transform.position.y > collisions.contacts[i].point.y)                  // if there are objects under player
+            {
+                in_air = false;             
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collisions)
+    {
+        in_air = true;                                                                  // set flag in air
     }
 }
