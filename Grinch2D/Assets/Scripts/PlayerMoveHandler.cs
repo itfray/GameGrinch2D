691b5f5586203b34c900d7201eb91281347e9aa3
Efarov Ilya <efarov.ily@mail.ru>
2021-01-14 17:04:42 +0900

    fix player collisions

--- Grinch2D/Assets/Scripts/PlayerMoveHandler.cs
+++ Grinch2D/Assets/Scripts/PlayerMoveHandler.cs
@@ -7,9 +7,9 @@ using UnityEngine;
 /// </summary>
 public class PlayerMoveHandler : JumpHandler
 {
-    private bool down_collision = false;                           // player have down collisions?
-    private bool left_collision = false;                           // player have left collisions?
-    private bool right_collision = false;
+    enum CollisionDirect { Left = 0, Right = 1, Down = 2 , Up = 3};
+    bool[] flags_collisions = new bool[4];                         // player have down, left, up, right collisions?
+    int[] counts_collisions = new int[4];                          // count collisions down, left, up, right that have player
     private bool is_capturing = false;                             // the player captures near the block
 
     /// <summary>
@@ -23,30 +23,33 @@ public class PlayerMoveHandler : JumpHandler
         if (IsCapturing())
         {
             // if the player presses the button in the opposite direction from the direction of movement
-            if ((left_collision && inputx > 0))
+            if ((flags_collisions[(int)CollisionDirect.Left] && inputx > 0))
             {
                 Jump();
+                flags_collisions[(int)CollisionDirect.Down] = false;
             }
-            else if (right_collision && inputx < 0)
+            else if (flags_collisions[(int)CollisionDirect.Right] && inputx < 0)
             {
                 Jump();
+                flags_collisions[(int)CollisionDirect.Down] = false;
             }
         }
 
         bool capturing = false;
-        if (down_collision)
+        if (flags_collisions[(int)CollisionDirect.Down])
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Jump();                                                             // pefrom jump
-                down_collision = false;
+                flags_collisions[(int)CollisionDirect.Down] = false;
             }
         }
         else
         {
             if (Input.GetKey(KeyCode.Space))
-            {
-                capturing = left_collision || right_collision;                      // condition for capturing
+            {                    
+                capturing = flags_collisions[(int)CollisionDirect.Left] ||          // condition for capturing
+                            flags_collisions[(int)CollisionDirect.Right];
             }
         }
 
@@ -56,6 +59,11 @@ public class PlayerMoveHandler : JumpHandler
         else
             Uncapture();
 
+        Debug.Log("count_left_collisions: " + counts_collisions[(int)CollisionDirect.Left].ToString());
+        Debug.Log("count_right_collisions: " + counts_collisions[(int)CollisionDirect.Right].ToString());
+        Debug.Log("count_down_collisions: " + counts_collisions[(int)CollisionDirect.Down].ToString());
+        Debug.Log("count_up_collisions: " + counts_collisions[(int)CollisionDirect.Up].ToString());
+        Debug.Log("===================================================================================");
         base.UpdateDirection();
     }
 
@@ -100,31 +108,100 @@ public class PlayerMoveHandler : JumpHandler
         return is_capturing;
     }
 
+    void unsetCountsCollisions()
+    {
+        for (int i = 0; i < counts_collisions.Length; i++)
+        {
+            counts_collisions[i] = 0;
+        }
+    }
+
+    void unsetFlagsCollisions()
+    {
+        for (int i = 0; i < flags_collisions.Length; i++)
+        {
+            flags_collisions[i] = false;
+        }
+    }
+
     void OnCollisionStay2D(Collision2D collisions)
     {
+        unsetCountsCollisions();
+        unsetFlagsCollisions();
+
         for (int i = 0; i < collisions.contactCount; i++)
         {
-            if (collisions.contacts[i].normal.y > 0)                  // if there are objects under player
+            if (collisions.GetContact(i).normal.y > 0)
             {
-                down_collision = true;
+                flags_collisions[(int)CollisionDirect.Down] = true;
+            }
+            else if (collisions.GetContact(i).normal.y < 0)
+            {
+                flags_collisions[(int)CollisionDirect.Up] = true;
             }
 
-            if (collisions.contacts[i].normal.x > 0)
+            if (collisions.GetContact(i).normal.x > 0)
             {
-                left_collision = true;
+                flags_collisions[(int)CollisionDirect.Left] = true;
             }
-            
-            if (collisions.contacts[i].normal.x < 0)
+            else if (collisions.GetContact(i).normal.x < 0)
             {
-                right_collision = true;
+                flags_collisions[(int)CollisionDirect.Right] = true;
+            }
+        }
+
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        Vector2 size2 = collider.size / 2 * transform.localScale;
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        Vector2[] direct_pos = new Vector2[counts_collisions.Length];
+        for (int i = 0; i < direct_pos.Length; i++)
+        {
+            direct_pos[i] = position + size2 * CollisionDirectToVector2((CollisionDirect)i);
+        }
+
+        for (int ic = 0; ic < collisions.contactCount; ic++)
+        {
+            Vector2[] diffs = new Vector2[counts_collisions.Length];
+
+            for (int i = 0; i < diffs.Length; i++)
+            {
+                diffs[i] = direct_pos[i] - collisions.GetContact(ic).point;
+                
+                if (i == (int)CollisionDirect.Up || i == (int)CollisionDirect.Down)
+                {
+                    if (Mathf.Abs(diffs[i].y) < Mathf.Abs(diffs[i].x))
+                        counts_collisions[i]++;
+                }
+
+                if (i == (int)CollisionDirect.Left || i == (int)CollisionDirect.Right)
+                {
+                    if (Mathf.Abs(diffs[i].y) > Mathf.Abs(diffs[i].x))
+                        counts_collisions[i]++;
+                }
             }
         }
     }
 
     void OnCollisionExit2D(Collision2D collisions)
     {
-        down_collision = false;                                     // unset all collisions flags
-        left_collision = false;
-        right_collision = false;
+        unsetCountsCollisions();
+        unsetFlagsCollisions();
+    }
+
+    static Vector2 CollisionDirectToVector2(CollisionDirect direct)
+    {
+        switch (direct)
+        {
+            case CollisionDirect.Left:
+                return Vector2.left;
+            case CollisionDirect.Right:
+                return Vector2.right;
+            case CollisionDirect.Down:
+                return Vector2.down;
+            case CollisionDirect.Up:
+                return Vector2.up;
+            default:
+                return Vector2.up;
+        }
     }
 }
