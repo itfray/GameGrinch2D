fbd66bc365dc4cdcc3328339f234351110939dba
Efarov Ilya <efarov.ily@mail.ru>
2021-01-26 00:29:29 +0900

    fix WeaponHandler

--- Grinch2D/Assets/Scripts/WeaponHandler.cs
+++ Grinch2D/Assets/Scripts/WeaponHandler.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
@@ -15,17 +15,33 @@ public class WeaponHandler : MonoBehaviour
 
     public bool canShoot { get{ return curRchrgTime <= 0f; } }                    // can shoot?
 
+    public int maxcBullets = 10;                                                  // max count bullets
+
+    private LinkedList<GameObject> bullets;                                      // list generated bullets
+
+    void Start()
+    {
+        bullets = new LinkedList<GameObject>();
+    }
+
     void Update()
     {
         if (curRchrgTime > 0)
             curRchrgTime -= Time.deltaTime;                                       // update current recharge time 
     }
 
+    /// <summary>
+    /// Method shoots from weapon
+    /// </summary>
+    /// <typeparam name="BulletHndT"> type bullet handler </typeparam>
+    /// <param name="inst_pos"> instantiate position for bullet </param>
+    /// <param name="shoot_direct"> shoot direction </param>
+    /// <returns> bullet </returns>
     public GameObject Attack<BulletHndT>(Vector2 inst_pos, Vector2 shoot_direct) where BulletHndT: BulletHandler
     {
         if (!canShoot || bulletPrefab == null) return null;                                                     // check possibility of a shot
 
-        curRchrgTime = rechrgTime;                                                                              // update current recharge time 
+        curRchrgTime = rechrgTime;                                                                              // update current recharge time
 
         GameObject bullet = Instantiate(bulletPrefab, inst_pos, Quaternion.identity, transform.parent);         // generate bullet
         BulletHndT bulletHndlr = bullet.GetComponent<BulletHndT>();                                             // get bullet handler
@@ -35,7 +51,25 @@ public class WeaponHandler : MonoBehaviour
             bulletHndlr.direction = shoot_direct;                                                               // set shoot direction
         }
         bullet.transform.eulerAngles = Vector3.forward * MathWay.calcAngle(shoot_direct);                       // set shoot angle
+
+        bullets.AddLast(bullet);
+
+        DestroyExtraBullets();                                                                                  // destroy old and extra bullets
+
         return bullet;
+    }
+
+    /// <summary>
+    /// Destroy old and extra bullets
+    /// </summary>
+    public void DestroyExtraBullets()
+    {
+        for (int i = 0; i < bullets.Count - maxcBullets; i++)                                                   // if maximum limit of bullets reached
+        {
+            GameObject bullet = bullets.FirstOrDefault();
+            if (bullet) Destroy(bullet, rechrgTime);
+            bullets.RemoveFirst();
+        }
     }
 }
 
