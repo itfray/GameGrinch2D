cc59855ab901e519ca8499b5d6b97665d5f3f8d6
Efarov Ilya <efarov.ily@mail.ru>
2021-01-25 13:50:35 +0900

    fix TurretBulletHnd

--- Grinch2D/Assets/Scripts/TurretBulletHnd.cs
+++ Grinch2D/Assets/Scripts/TurretBulletHnd.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TurretBulletHnd : BulletHandler
 {
     public float ddirect = 0.1f;
+    public float dangle = 0.1f;
     public GameObject target;
 
     protected override void UpdateDirection()
@@ -14,6 +15,9 @@ public class TurretBulletHnd : BulletHandler
         Vector2 direct = MathWay.calcDirect(new Vector2(transform.position.x, transform.position.y),
                                             new Vector2(target.transform.position.x, target.transform.position.y));
         direction = Vector2.Lerp(direction, direct, ddirect);
-        transform.eulerAngles = Vector3.forward * MathWay.calcAngle(direction);
+        float angle = MathWay.calcAngle(Vector2.Lerp(new Vector2(Mathf.Cos(transform.eulerAngles.z * Mathf.Deg2Rad),
+                                                                 Mathf.Sin(transform.eulerAngles.z * Mathf.Deg2Rad)),
+                                                     direct, dangle));
+        transform.eulerAngles = Vector3.forward * angle;
     }
 }
